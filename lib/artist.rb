33accf7c46e845dce26ed9ea27bf1069f1f29028
Diego Suarez and Kevin Finley <kevin@>
2016-11-30 12:47:38 -0800

    add ablum_name

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,8 @@
 class Artist
   attr_reader(:artist_name, :ablum_name, :gene)
 
-  def initialize(artist_name)
-    @artist_name = artist_name
+  def initialize(attributes)
+    @artist_name = attributes.fetch(:artist_name, "Not Available")
+    @ablum_name = attributes.fetch(:ablum_name, "Not Available")
   end
 end
--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -4,8 +4,9 @@ require('artist')
 describe(Artist) do
   describe('#initialize') do
     it('get info for cd') do
-      test_artist = Artist.new('Kevin')
+      test_artist = Artist.new(:artist_name => 'Kevin', :ablum_name => "Computer")
       expect(test_artist.artist_name()).to(eq("Kevin"))
+      expect(test_artist.ablum_name()).to(eq("Computer"))
     end
   end
 end
